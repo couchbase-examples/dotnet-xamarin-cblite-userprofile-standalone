bdb52b11b4ce9a83f858e74c5d74cbf9b39b6861
Aaron LaBeau <alabeau@gmail.com>
2022-02-12 15:08:05 -0500

    updated for unit tests for Android

---
+++ src/UserProfileDemo.UITests/AppInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UserProfileDemo.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+                path = path.Replace(@"\UserProfileDemo.UITests\bin\Debug", "");
+                path = path.Replace(@"file:\", "");
+                path = $"{path}\\UserProfileDemo.Android\\bin\\debug\\com.couchbase.userprofiledemo.apk";
+
+                return ConfigureApp
+                    .Android
+                    .ApkFile(path)
+                    .StartApp();
+            }
+           
+            return ConfigureApp
+                .iOS
+                .AppBundle("../UserProfileDemo.iOS/bin/iPhone/Debug/UserProfileDemo.app")
+                .StartApp();
+        }
+    }
+}
---
+++ src/UserProfileDemo.UITests/TestHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProfileDemo.UITests
+{
+    internal static class TestHelper
+    {
+        public static string USERNAMEFIELD = "eUsername";
+        public static string PASSWORDFIELD = "ePassword";
+        public static string NAMEFIELD = "eFullName";
+        public static string ADDRESSFIELD = "eAddress";
+        public static string EMAILFIELD = "eEmailAddress";
+        public static string SIGNINBUTTON = "btnSignIn";
+        public static string SIGNOUTBUTTON = "btnSignOut";
+        public static string SAVEBUTTON = "btnSave";
+        public static string OKBUTTONANDROID = "OK";
+        public static string PROFILEHEADER = "Your Profile";
+    }
+
+    internal static class TestData
+    {
+        public static string TESTUSER1 = "demo@example.com";
+        public static string TESTUSER2 = "demo2@example.com";
+        public static string TESTPASSWORD = "password";
+        public static string TESTADDRESS = "123 Nowhere Street";
+        public static string TESTNAME = "John Doe";
+
+    }
+}
