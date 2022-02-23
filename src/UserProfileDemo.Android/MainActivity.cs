21ef2f8989682eb7728523f00c91c0ad23a947de
Aaron LaBeau <alabeau@gmail.com>
2022-02-23 12:24:46 -0500

    update to cb lite 3.0

--- src/UserProfileDemo.Android/MainActivity.cs
+++ src/UserProfileDemo.Android/MainActivity.cs
@@ -10,7 +10,8 @@ namespace UserProfileDemo.Droid
         Theme = "@style/MainTheme",
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity 
+	: global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
